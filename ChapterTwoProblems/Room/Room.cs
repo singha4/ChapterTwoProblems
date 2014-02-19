a7cc48e15361cf04250ce5c8e09eb11038f7680b
singha4 <amandeep159632478@gmail.com>
2014-02-19 10:04:13 -0500

    Version 1

---
+++ ChapterTwoProblems/Room/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Room
+{
+    class Room
+    {
+        static void Main(string[] args)
+        {
+            int length = 15, width = 25, area;
+            area = length * width;
+            Console.WriteLine("The floor space is {0} square feet.", area);
+        }
+    }
+}
